dffea0fae08cf484d7aaf4d5272f0fc0fdaf60b7
Yuri Tkachenko <yura.tkachenko@gmail.com>
2024-04-17 23:45:42 -0700

    #3: rewrite 'writer' using syscalss

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -7,8 +7,8 @@ set -u
 
 OUTDIR=/tmp/aeld
 KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
-KERNEL_VERSION=v5.1.10
-BUSYBOX_VERSION=1_33_1
+KERNEL_VERSION=v6.8.6
+BUSYBOX_VERSION=1_36_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
@@ -34,10 +34,15 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -47,7 +52,12 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+# Create necessary base directories
+mkdir ${OUTDIR}/rootfs
+cd ${OUTDIR}/rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -55,26 +65,58 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    # Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+# Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+
+cd ${OUTDIR}/rootfs
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+# Add library dependencies to rootfs
+TOOLCHAIN_DIR=$(dirname $(dirname $(which ${CROSS_COMPILE}readelf)))
+BB_LIBS="$(${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter" | grep -Po 'program interpreter: \K[^]]*')"
+echo "$BB_LIBS" | while IFS= read -r lib_file; do
+    find $TOOLCHAIN_DIR -name "$(basename $lib_file)" | xargs -I {} cp {} ${OUTDIR}/rootfs$(dirname $lib_file)
+done
+# shared library
+BB_LIBS="$(${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library" | grep -Po 'Shared library: \[\K[^]]*')"
+echo "$BB_LIBS" | while IFS= read -r lib_file; do
+    find $TOOLCHAIN_DIR -name "$(basename $lib_file)" | xargs -I {} cp {} ${OUTDIR}/rootfs/lib64
+done
 
-# TODO: Make device nodes
+# Make device nodes
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/console c 5 1
+# Clean and build the writer utility
+cd ${FINDER_APP_DIR}
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
 
-# TODO: Clean and build the writer utility
-
-# TODO: Copy the finder related scripts and executables to the /home directory
+# Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp ${FINDER_APP_DIR}/writer ${OUTDIR}/rootfs/home
+cp ${FINDER_APP_DIR}/finder.sh ${OUTDIR}/rootfs/home
+cp ${FINDER_APP_DIR}/finder-test.sh ${OUTDIR}/rootfs/home
+mkdir -p ${OUTDIR}/rootfs/home/conf
+cp ${FINDER_APP_DIR}/conf/assignment.txt ${OUTDIR}/rootfs/home/conf/assignment.txt
+cp ${FINDER_APP_DIR}/conf/username.txt ${OUTDIR}/rootfs/home/conf/username.txt
 
-# TODO: Chown the root directory
 
-# TODO: Create initramfs.cpio.gz
+# Chown the root directory
+cd ${OUTDIR}/rootfs
+sudo chown -R root:root *
+
+# Create initramfs.cpio.gz
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd ${OUTDIR}
+gzip -f initramfs.cpio
--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,7 +1,8 @@
-#include <stdio.h>
 #include <string.h>
 #include <syslog.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int exitWriter(int code) {
     closelog();
@@ -13,7 +14,7 @@ int main(int argc, char** argv) {
     openlog(NULL, LOG_ODELAY, LOG_USER);
 
     if (argc <= 2) {
-        printf("Usage: write <FILE> <STRING>\n");
+        syslog(LOG_ERR, "Usage: write <FILE> <STRING>");
 
         return exitWriter(1);
     }
@@ -22,12 +23,12 @@ int main(int argc, char** argv) {
     const char* text = argv[2];
 
     if (strlen(filename) == 0) {
-        fprintf(stderr, "Required parameter 'FILE' is blank.\n");
+        syslog(LOG_ERR, "Required parameter 'FILE' is blank.");
 
         return exitWriter(1);
     }
     if (strlen(text) == 0) {
-        fprintf(stderr, "Required parameter 'STRING' is blank.\n");
+        syslog(LOG_ERR, "Required parameter 'STRING' is blank.");
 
         return exitWriter(1);
     }
@@ -35,15 +36,15 @@ int main(int argc, char** argv) {
     syslog(LOG_DEBUG, "Writing <%s> to <%s>", text, filename);
 
     int exitCode = 0;
-    FILE* f = fopen(filename, "w");
-    if (f != NULL) {
-        size_t result = fwrite(text, strlen(text), 1, f);
-        if (result == 0) {
+    int fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY);
+    if (fd > 0) {
+        ssize_t result = write(fd, text, strlen(text));
+        if (result == -1) {
             syslog(LOG_ERR, "Failure to write file: %s", strerror(errno));
             exitCode = 1;
         }
 
-        fclose(f);
+        close(fd);
     } else {
         syslog(LOG_ERR, "Failure to write file: %s", strerror(errno));
     }
